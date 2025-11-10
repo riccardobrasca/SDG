5109f5ea262600d114f76d38c61fa3c4b8935228
Riccardo Brasca <riccardo.brasca@gmail.com>
2025-11-10 17:52:29 +0100

    better

--- SDG/Synthetic.lean
+++ SDG/Synthetic.lean
@@ -1,6 +1,7 @@
 import Mathlib.Algebra.DualNumber
 import Mathlib.Algebra.Algebra.Pi
 
+axiom unique_choice {α : Type*} [Unique α] : α
 namespace SDG
 
 open DualNumber Function
@@ -11,7 +12,7 @@ abbrev D : Subsemigroup R where
  carrier := {(x : R) | x ^ 2 = 0}
  mul_mem' := fun hx hy ↦ by simp_all [mul_pow]
 
-lemma zero_mem_D : (0 : R) ∈ D R := by
+lemma zero_mem_D : 0 ∈ D R := by
   show 0 ^ 2 = 0
   rw [sq, mul_zero]
 
@@ -24,18 +25,21 @@ lemma coe_sq : ((↑) : D R → R) * (↑) = 0 := by
   ext d
   simpa only [← pow_two, Pi.zero_apply] using d.2
 
+variable {R}
+
 def α : DualNumber R →ₐ[R] (D R → R) :=
   lift ⟨⟨Algebra.ofId _ _, (↑)⟩, coe_sq _, fun _ ↦ Commute.all _ _⟩
 
-@[simp] lemma α_apply (a b : R) (d : D R) : α R ⟨a, b⟩ d = a + d * b := by
+@[simp] lemma α_apply (a b : R) (d : D R) : α ⟨a, b⟩ d = a + d * b := by
   simp [α, lift_apply_apply, mul_comm]
 
 section IsKockLawvere
 
+variable (R) in
 class IsKockLawvere extends Nontrivial R where
   isKockLawvere : ∀ g : D R → R, ∃! b, ∀ d, g d = g 0 + d * b
 
-variable [IsKockLawvere R] {R}
+variable [IsKockLawvere R]
 
 lemma exists_derivative (g : D R → R) : ∃! b, ∀ d, g d = g 0 + d * b :=
   IsKockLawvere.isKockLawvere g
@@ -62,7 +66,7 @@ lemma cancel_d (b₁ b₂ : R) (h : ∀ d ∈ D R, d * b₁ = d * b₂) : b₁ =
   intro d
   simp [g2, (h d d.2).symm, h1]
 
-lemma injective_α : Injective (α R) := by
+lemma injective_α : Injective (α (R := R)) := by
   intro ⟨x, y⟩ ⟨z, t⟩ h
   have hxz := congr_fun h 0
   simp at hxz
@@ -75,21 +79,21 @@ lemma injective_α : Injective (α R) := by
       simpa [hxz]
     exact cancel_d _ _ h
 
-lemma surjective_α : Surjective (α R) := by
+lemma surjective_α : Surjective (α (R := R)) := by
   intro f
   obtain ⟨b, hb, unique⟩ := exists_derivative f
   use ⟨f 0, b⟩
   ext d
   simp [hb d]
 
-lemma bijective_α : Bijective (α R) :=
+lemma bijective_α : Bijective (α (R := R)) :=
   ⟨injective_α, surjective_α⟩
 
 end IsKockLawvere
 
 class KockLawvere extends Nontrivial R where
   inv : (D R → R) →ₐ[R] DualNumber R
-  left : LeftInverse (α R) inv
-  right : RightInverse (α R) inv
+  left : LeftInverse α inv
+  right : RightInverse α inv
 
 end SDG
