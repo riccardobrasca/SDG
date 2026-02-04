a29a0460efc5eee69030af8f004cc703cff53f38
Riccardo Brasca <riccardo.brasca@gmail.com>
2026-02-04 17:48:23 +0100

    we need this

---
+++ SDG/Basic/FactorialInv.lean
@@ -0,0 +1,27 @@
+import Mathlib
+
+import SDG.Linters.choice
+
+namespace SDG
+
+open Nat
+
+variable (R : Type*) [CommRing R]
+
+example : Invertible (1 : R) := by
+  exact invertibleOne
+
+class Divisible where
+  divisible : ∀ {n : ℕ}, n ≠ 0 → Invertible (n : R)
+
+variable [Divisible R]
+
+instance (n : ℕ) : Invertible (n ! : R) :=
+  Divisible.divisible (factorial_ne_zero n)
+
+@[simp]
+lemma inv_zero_factorial : ⅟(0! : R) = 1 := by
+  have : Invertible (1 : R) := invertibleOne
+  simp
+
+end SDG
