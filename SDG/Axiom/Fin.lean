9740cbb5db8fe6dcfefb505f7669e1b58a9b4664
Riccardo Brasca <riccardo.brasca@gmail.com>
2026-02-09 13:23:00 +0100

    bump

--- SDG/Axiom/Fin.lean
+++ SDG/Axiom/Fin.lean
@@ -6,55 +6,49 @@ import Mathlib.Algebra.BigOperators.Group.Finset.Basic
 
 namespace SDG
 
-section List
+namespace List
 
 open List
 
 theorem pairwise_lt_range' {s n} (step := 1) (pos : 0 < step := by simp) :
-    Pairwise (· < ·) (range' s n step) :=
+    List.Pairwise (· < ·) (List.range' s n step) :=
   match s, n, step, pos with
-  | _, 0, _, _ => Pairwise.nil
+  | _, 0, _, _ => List.Pairwise.nil
   | s, n + 1, step, pos => by
-    simp only [range'_succ, pairwise_cons]
+    simp only [List.range'_succ, List.pairwise_cons]
     constructor
     · intro n m
-      obtain ⟨a, -, rfl⟩ := mem_range'.1 m
+      obtain ⟨a, -, rfl⟩ := List.mem_range'.1 m
       calc s < s + step := lt_add_of_pos_right _ pos
            _ ≤ s + step + step * a := Nat.le_add_right _ _
     · exact pairwise_lt_range' (s := s + step) step pos
 
 theorem pairwise_le_range' {s n} (step := 1) :
-    Pairwise (· ≤ ·) (range' s n step) :=
+    List.Pairwise (· ≤ ·) (List.range' s n step) :=
   match s, n, step with
-  | _, 0, _ => Pairwise.nil
+  | _, 0, _ => List.Pairwise.nil
   | s, n + 1, step => by
-    simp only [range'_succ, pairwise_cons]
+    simp only [List.range'_succ, List.pairwise_cons]
     constructor
     · intro n m
-      obtain ⟨a, -, rfl⟩ := mem_range'.1 m
+      obtain ⟨a, -, rfl⟩ := List.mem_range'.1 m
       rw [add_assoc]
       exact Nat.le_add_right s (step + step * a)
     · exact pairwise_le_range' (s := s + step) step
 
-theorem nodup_range' {s n : Nat} (step := 1) (h : 0 < step := by simp) : Nodup (range' s n step) :=
+theorem nodup_range' {s n : Nat} (step := 1) (h : 0 < step := by simp) :
+    List.Nodup (List.range' s n step) :=
   (pairwise_lt_range' step h).imp Nat.ne_of_lt
 
-theorem nodup_range {n : Nat} : Nodup (range n) := by
-  simp +decide only [range_eq_range', nodup_range']
+theorem nodup_range {n : Nat} : List.Nodup (List.range n) := by
+  simp +decide only [List.range_eq_range', nodup_range']
 
-theorem nodup_finRange (n) : (finRange n).Nodup := by
-  rw [finRange_eq_pmap_range]
-  exact (Pairwise.pmap nodup_range _) fun _ _ _ _ => @Fin.ne_of_val_ne _ ⟨_, _⟩ ⟨_, _⟩
+theorem nodup_finRange (n) : (List.finRange n).Nodup := by
+  rw [List.finRange_eq_pmap_range]
+  exact (List.Pairwise.pmap nodup_range _) fun _ _ _ _ => @Fin.ne_of_val_ne _ ⟨_, _⟩ ⟨_, _⟩
 
 instance Fin.fintype (n : ℕ) : Fintype (Fin n) :=
-  ⟨⟨finRange n, nodup_finRange n⟩, List.mem_finRange⟩
-
-theorem Fin.univ_def (n : ℕ) : (Finset.univ : Finset (Fin n)) = ⟨finRange n, nodup_finRange n⟩ :=
-  rfl
-
-@[simp] theorem Fin.univ_val_map {α : Type*} {n : ℕ} (f : Fin n → α) :
-    Finset.univ.val.map f = List.ofFn f := by
-  simp [List.ofFn_eq_map, univ_def]
+  ⟨⟨List.finRange n, nodup_finRange n⟩, List.mem_finRange⟩
 
 end List
 section BigOperators
@@ -64,6 +58,13 @@ namespace Fin
 open Finset
 
 variable {n : ℕ} {M : Type*} [CommMonoid M] {A : Type*} [AddCommMonoid A]
+
+theorem univ_def (n : ℕ) :
+  (Finset.univ : Finset (Fin n)) = ⟨List.finRange n, List.nodup_finRange n⟩ := rfl
+
+@[simp] theorem univ_val_map {α : Type*} {n : ℕ} (f : Fin n → α) :
+    Finset.univ.val.map f = List.ofFn f := by
+  simp [List.ofFn_eq_map, univ_def]
 
 @[to_additive]
 theorem prod_ofFn (f : Fin n → M) : (List.ofFn f).prod = ∏ i, f i := by
