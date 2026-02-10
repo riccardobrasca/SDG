a22ccc4fe1c2a224155730c77145f2172b46d3e3
Riccardo Brasca <riccardo.brasca@gmail.com>
2026-02-10 10:56:41 +0100

    add this

--- SDG/IsKockLawvere_one/Basic.lean
+++ SDG/IsKockLawvere_one/Basic.lean
@@ -4,13 +4,24 @@ namespace SDG
 
 variable {R : Type*} [CommRing R] [IsKockLawvere_one R]
 
-open IsKockLawvere_one
+open IsKockLawvere_one BigOperators
 
 lemma cancel_d {b₁ b₂ : R} (h : ∀ (d : D R), b₁ * d = b₂ * d) : b₁ = b₂ := by
   obtain ⟨b1, -, unique1⟩ := isKockLawvere_one (b₁ * · : D R → R)
   obtain ⟨b2, -, unique2⟩ := isKockLawvere_one (b₂ * · : D R → R)
   rw [unique1 b₁ (fun d ↦ by simp), unique2 b₂ (fun d ↦ by simp)]
   exact unique2 _ (fun d ↦ by simp [(h d).symm, unique1 b₁ (fun d ↦ by simp)])
+
+lemma cancel_d_fun {b₁ b₂ : R} : ∀ (k : ℕ),
+  (∀ (d : Fin k → D R), b₁ * ∏ i, (d i).1 = b₂ * ∏ i, (d i).1) → b₁ = b₂
+| 0 => fun h ↦ by simpa only [Fin.prod_univ_zero, mul_one] using h 0
+| k + 1 => fun h ↦ by
+  refine cancel_d_fun k (fun D ↦ cancel_d (fun d ↦ ?_))
+  rw [mul_assoc, mul_assoc, mul_comm _ d.1, ← Fin.prod_snoc]
+  have := h (Fin.snoc (fun i ↦ D i) d)
+  convert this with i _ i <;>
+  rcases Fin.eq_castSucc_or_eq_last i with ⟨j, rfl⟩ | rfl <;>
+  simp
 
 variable (R) in
 lemma D_neq_zero : ¬(∀ d ∈ D R, d = 0) := by
