734d44b578aa4633651f8915c3eb350368888b8c
Riccardo Brasca <riccardo.brasca@gmail.com>
2026-02-18 16:21:56 +0100

    better notation

--- SDG/IsKockLawvere/Taylor.lean
+++ SDG/IsKockLawvere/Taylor.lean
@@ -11,22 +11,22 @@ set_option backward.isDefEq.respectTransparency false in
 theorem taylor_two [Invertible (2 : R)] (f : R → R) (x : R) (δ : 𝔻 R 2) :
     f (x + δ) = f x + ∂f x * δ + ∂∂f x * δ ^ 2 * ⅟2 := by
   let g_x : 𝔻 R 2 → R := fun d ↦ f (x + d)
-  obtain ⟨B, hB, -⟩ := isKockLawvere 2 g_x
+  obtain ⟨b, hb, -⟩ := isKockLawvere 2 g_x
   simp only [coe_zero, add_zero, Fin.sum_univ_two, Fin.isValue, Fin.coe_ofNat_eq_mod,
     Nat.zero_mod, zero_add, pow_one, Nat.mod_succ, Nat.reduceAdd, Subtype.forall,
-    Subsemigroup.mem_mk, Set.mem_setOf_eq, g_x] at hB
-  have hB_deriv : ∂f x = B 0 := derivative_unique
-    (fun d ↦ by simpa using hB _ (𝔻_le (by decide) d.2))
-  have : ∀ (d₁ d₂ : D R), B 1 * 2 * d₁ * d₂ = ∂∂f x * d₁ * d₂ := by
+    Subsemigroup.mem_mk, Set.mem_setOf_eq, g_x] at hb
+  have hB_deriv : ∂f x = b 0 := derivative_unique
+    (fun d ↦ by simpa using hb _ (𝔻_le (by decide) d.2))
+  have : ∀ (d₁ d₂ : D R), b 1 * 2 * d₁ * d₂ = ∂∂f x * d₁ * d₂ := by
     intro d₁ d₂
-    specialize hB (d₁ + d₂) (mem_𝔻_of_mem_D_add_mem_D _ _)
+    specialize hb (d₁ + d₂) (mem_𝔻_of_mem_D_add_mem_D _ _)
     rw [taylor_two_aux f x d₁ d₂, ← hB_deriv, add_assoc, D_add_sq, ← mul_assoc, ← mul_assoc,
-      ← mul_assoc, ← mul_assoc, add_right_inj, add_right_inj] at hB
-    rw [← hB, mul_assoc, mul_assoc, mul_assoc, mul_assoc, mul_comm 2, mul_assoc, mul_assoc]
+      ← mul_assoc, ← mul_assoc, add_right_inj, add_right_inj] at hb
+    rw [← hb, mul_assoc, mul_assoc, mul_assoc, mul_assoc, mul_comm 2, mul_assoc, mul_assoc]
     simp
-  rw [hB _ δ.2, add_assoc, hB_deriv]
+  rw [hb _ δ.2, add_assoc, hB_deriv]
   congr 2
-  simp [((cancel_d (fun _ ↦ cancel_d (fun _ ↦ this _ _))).symm : ∂∂f x = B 1 * 2), mul_assoc,
+  simp [((cancel_d (fun _ ↦ cancel_d (fun _ ↦ this _ _))).symm : ∂∂f x = b 1 * 2), mul_assoc,
     mul_comm ((δ : R) ^ 2)]
 
 set_option backward.isDefEq.respectTransparency false in
