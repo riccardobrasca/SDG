3125d44081582e6d307c68066da0e90ef119d740
Riccardo Brasca <riccardo.brasca@gmail.com>
2026-02-20 13:43:07 +0100

    better

--- SDG/Axiom/Function.lean
+++ SDG/Axiom/Function.lean
@@ -2,24 +2,24 @@ import Mathlib.Logic.Function.Basic
 
 import SDG.Linters.choice
 
+open Function
 namespace SDG.Function
 
 variable {α β : Type*} [DecidableEq α] (f : α → β) (a : α)
 
 @[simp]
-lemma update_eq_self : Function.update f a (f a) = f := by
+lemma update_eq_self : update f a (f a) = f := by
   ext x
   by_cases hx : x = a
-  · rw [hx, Function.update_self]
-  · rw [Function.update_of_ne hx]
+  · rw [hx, update_self]
+  · rw [update_of_ne hx]
 
 lemma update_update_comm (f : α → β) {a₁ a₂ : α} (h : a₁ ≠ a₂) (b₁ b₂ : β) :
-    Function.update (Function.update f a₁ b₁) a₂ b₂ =
-      Function.update (Function.update f a₂ b₂) a₁ b₁ := by
+    update (update f a₁ b₁) a₂ b₂ = update (update f a₂ b₂) a₁ b₁ := by
   ext a
   rcases Decidable.eq_or_ne a a₁ with rfl | h
-  · simp [Function.update, h]
+  · simp [update, h]
   · by_cases h' : a = a₂ <;>
-    simp_all [Function.update]
+    simp_all [update]
 
 end SDG.Function
