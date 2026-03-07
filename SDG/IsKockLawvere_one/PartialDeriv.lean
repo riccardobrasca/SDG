d2aafdfd04b423ac74a18e3955d724aaa3537ec5
Riccardo Brasca <riccardo.brasca@gmail.com>
2026-03-07 20:57:51 +0000

    add partial_deriv_iterate_eq_deriv_snoc_init

--- SDG/IsKockLawvere_one/PartialDeriv.lean
+++ SDG/IsKockLawvere_one/PartialDeriv.lean
@@ -3,8 +3,9 @@ import Mathlib.Algebra.BigOperators.Group.Finset.Powerset
 
 import SDG.Basic.D
 import SDG.IsKockLawvere_one.Basic
+import SDG.IsKockLawvere_one.Deriv
 
-open Function SDG.IsKockLawvere_one Finset
+open Function SDG.IsKockLawvere_one Finset Fin
 
 namespace SDG
 
@@ -24,6 +25,7 @@ lemma partial_deriv_propr : ∀ (x : Fin n → R), ∃! b, ∀ (d : D R),
     rw [coe_zero, add_zero, update_eq_self]
   · convert hb₁ d
     rw [coe_zero, add_zero, update_eq_self]
+
 
 noncomputable def partial_derivFun : (Fin n → R) → R :=
   unique_choice_fun (partial_deriv_propr i f)
@@ -62,8 +64,8 @@ instance : FunLike (Derivation R ((Fin n → R) → R) ((Fin n → R) → R))
   coe D := D.toFun
   coe_injective' := DFunLike.coe_injective
 
-notation3:max "∂_[" i "]" f:max => partial_deriv i f
-notation3:max "∂_[" i "]^[" n "]" f:max => (partial_deriv i)^[n] f
+notation3:max "∂_[" i "]" f:max => partial_derivFun i f
+notation3:max "∂_[" i "]^[" n "]" f:max => (partial_derivFun i)^[n] f
 
 variable (x)
 
@@ -75,13 +77,41 @@ lemma partial_derivative_unique {b : R} (hb : ∀ (d : D R), f (update x i (x i 
     ∂_[i]f x = b :=
   partial_derivFun_unique i  hb
 
+lemma partial_deriv_eq_deriv (r : Fin n → R) :
+    ∂_[i] f r = ∂(fun y ↦ f (Function.update r i y)) (r i) := by
+  apply cancel_d; intro d
+  have h1 := partial_taylor_one i f r d
+  have h2 : f (Function.update r i (r i + ↑d)) =
+            f r + ∂(fun y ↦ f (Function.update r i y)) (r i) * ↑d := by
+    have := taylor_one (fun y ↦ f (Function.update r i y)) (r i) d
+    simp only [Function.update_eq_self] at this
+    exact this
+  exact add_left_cancel (h1.symm.trans h2)
+
+lemma partial_deriv_eq_deriv_snoc_init (f : (Fin (n + 1) → R) → R) (r : Fin (n + 1) → R) :
+    ∂_[last n] f r = ∂(fun t ↦ f (snoc (init r) t)) (r (last n)) := by
+  convert partial_deriv_eq_deriv .. using 2
+  ext; congr; ext j
+  rcases eq_castSucc_or_eq_last j with ⟨k, rfl⟩ | rfl <;>
+  simp [init]
+
+lemma partial_deriv_iterate_eq_deriv_snoc_init (k : ℕ) (f : (Fin (n + 1) → R) → R)
+  (r : Fin (n + 1) → R) :
+    ∂_[last n]^[k] f r = ∂^[k] (fun t ↦ f (snoc (init r) t)) (r (last n)) := by
+match k with
+| 0 => simp [snoc_init_self]
+| k + 1 =>
+  simp only [Function.iterate_succ', Function.comp_def]
+  simp [partial_deriv_eq_deriv_snoc_init, partial_deriv_iterate_eq_deriv_snoc_init]
+
 @[simp]
 theorem partial_deriv_const (r : R) : ∂_[i](fun _ ↦ r) = 0 :=
   funext fun _ ↦ partial_derivative_unique i (fun d ↦ by simp)
 
 theorem partial_deriv_mul (f g : (Fin n → R) → R) :
     ∂_[i](f * g) = ∂_[i]f * g + f * ∂_[i]g := by
-  simp; ring
+  change ⇑(partial_deriv i) (f * g) = ⇑(partial_deriv i) f * g + f * ⇑(partial_deriv i) g
+  simp [smul_eq_mul]; ring
 
 theorem partial_deriv_comm (i j : Fin n) : ∂_[i](∂_[j]f) x = ∂_[j](∂_[i]f) x := by
   by_cases H : i = j
@@ -108,38 +138,38 @@ theorem partial_deriv_proj_ne {i j : Fin n} (hij : i ≠ j) : ∂_[i](fun x : Fi
 
 theorem prop41_ex : ∀ n (τ : (Fin n → D R) → R), ∃ (a : Finset (Fin n) → R), ∀ (d : Fin n → D R),
     τ d = ∑ H, a H * ∏ j ∈ H, (d j : R)
-| 0 => fun τ ↦ ⟨fun _ ↦ τ Fin.elim0, fun d ↦ by
+| 0 => fun τ ↦ ⟨fun _ ↦ τ elim0, fun d ↦ by
     rw [← singleton_eq_univ, sum_singleton, prod_empty, mul_one]
-    exact (funext_iff_of_subsingleton d Fin.elim0).2 rfl⟩
+    exact (funext_iff_of_subsingleton d elim0).2 rfl⟩
 | n + 1 => fun τ ↦ by
-  let τ0 := fun d ↦ τ (Fin.cons 0 d)
+  let τ0 := fun d ↦ τ (cons 0 d)
   let h₁ : ∀ d : Fin n → D R, ∃! b : R, ∀ d₀ : D R,
-    τ (Fin.cons d₀ d) = τ (Fin.cons 0 d) + b * d₀ := fun d ↦
-    isKockLawvere_one (fun d₀ : D R ↦ τ (Fin.cons d₀ d))
+    τ (cons d₀ d) = τ (cons 0 d) + b * d₀ := fun d ↦
+    isKockLawvere_one (fun d₀ : D R ↦ τ (cons d₀ d))
   let τ1 := unique_choice_fun h₁
-  have hτ1 : ∀ d d₀, τ (Fin.cons d₀ d) = τ (Fin.cons 0 d) + τ1 d * d₀ := unique_choice_fun_spec h₁
+  have hτ1 : ∀ d d₀, τ (cons d₀ d) = τ (cons 0 d) + τ1 d * d₀ := unique_choice_fun_spec h₁
   obtain ⟨a0, ha0⟩ := prop41_ex _ τ0
   obtain ⟨a1, ha1⟩ := prop41_ex _ τ1
-  let up := map ⟨Fin.succ, Fin.succ_injective n⟩
+  let up := map ⟨succ, succ_injective n⟩
   let down : Finset (Fin (n + 1)) → Finset (Fin n) := fun H ↦ {i | i.succ ∈ H}
   let a := fun H ↦ if 0 ∈ H then a1 (down H) else a0 (down H)
   refine ⟨a, fun d ↦ ?_⟩
   let rest : Fin n → D R := fun i ↦ d i.succ
-  have h0 : τ (Fin.cons 0 rest) = ∑ H, a0 H * ∏ j ∈ H, (rest j : R) := ha0 rest
+  have h0 : τ (cons 0 rest) = ∑ H, a0 H * ∏ j ∈ H, (rest j : R) := ha0 rest
   have h1 : τ1 rest = ∑ H, a1 H * ∏ j ∈ H, (rest j : R) := ha1 rest
   have hs : 0 ∉ up univ := fun h ↦ by
-    rcases mem_map.mp h with ⟨i, -, hi⟩; exact Fin.succ_ne_zero i hi
+    rcases mem_map.mp h with ⟨i, -, hi⟩; exact succ_ne_zero i hi
   have huniv : insert 0 (up univ) = univ := by
-    ext i; rcases Fin.eq_zero_or_eq_succ i with rfl | ⟨j, rfl⟩ <;> simp [up]
+    ext i; rcases eq_zero_or_eq_succ i with rfl | ⟨j, rfl⟩ <;> simp [up]
   have hup_inj : Function.Injective up := by
-    simpa [up] using (map_injective ⟨Fin.succ, Fin.succ_injective n⟩)
+    simpa [up] using (map_injective ⟨succ, succ_injective n⟩)
   have hpow : (up univ).powerset = univ.image up := by
-    have hUpImage : (fun H ↦ H.image Fin.succ) = up := by
+    have hUpImage : (fun H ↦ H.image succ) = up := by
       ext; simp [up, map_eq_image]
     calc
-      (up univ).powerset = ((univ.image Fin.succ)).powerset := by simp [up, map_eq_image]
-      _ = univ.image (fun H : Finset (Fin n) ↦ H.image Fin.succ) := by
-            simpa using (powerset_image (s := univ) (f := Fin.succ))
+      (up univ).powerset = ((univ.image succ)).powerset := by simp [up, map_eq_image]
+      _ = univ.image (fun H : Finset (Fin n) ↦ H.image succ) := by
+            simpa using (powerset_image (s := univ) (f := succ))
       _ = univ.image up := by simp [hUpImage]
   have hprod_up : ∀ H : Finset (Fin n), (∏ j ∈ up H, (d j : R)) = ∏ j ∈ H, (rest j : R) :=
     fun H ↦ by simp [up, rest]
@@ -157,14 +187,14 @@ theorem prop41_ex : ∀ n (τ : (Fin n → D R) → R), ∃ (a : Finset (Fin n) 
         refine sum_congr rfl (fun H hH ↦ ?_)
         have h0not : 0 ∉ up H := fun h ↦ by
          rcases mem_map.mp h with ⟨i, -, hi⟩
-         exact Fin.succ_ne_zero i hi
+         exact succ_ne_zero i hi
         simp [mon, a, up, down, hprod_up H, h0not, prod_insert, mul_comm, mul_left_comm]
       · intro s hs t ht hst
         exact hup_inj hst
-  have hcons : d = Fin.cons (d 0) rest := by
-    ext i; rcases Fin.eq_zero_or_eq_succ i with rfl | ⟨j, rfl⟩ <;> simp [rest]
+  have hcons : d = cons (d 0) rest := by
+    ext i; rcases eq_zero_or_eq_succ i with rfl | ⟨j, rfl⟩ <;> simp [rest]
   calc
-    τ d = τ (Fin.cons 0 rest) + τ1 rest * d 0 := by rw [hcons]; simpa [rest] using hτ1 rest (d 0)
+    τ d = τ (cons 0 rest) + τ1 rest * d 0 := by rw [hcons]; simpa [rest] using hτ1 rest (d 0)
     _ = ∑ H, a0 H * ∏ j ∈ H, (rest j : R) + (∑ H, a1 H * ∏ j ∈ H, (rest j : R)) * d 0 := by
       rw [h0, h1]
     _ = ∑ H ∈ (up univ).powerset, mon H + (∑ H ∈ (up univ).powerset, mon (insert 0 H)) := hsum_repr
