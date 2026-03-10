aaffde760b726bbe62dde1daab904798c4bcd3ab
Riccardo Brasca <riccardo.brasca@gmail.com>
2026-03-10 11:57:09 +0000

    done modulo choice

--- SDG/IsKockLawvere/TaylorMulti.lean
+++ SDG/IsKockLawvere/TaylorMulti.lean
@@ -9,51 +9,131 @@ namespace SDG
 
 variable {R : Type*} [CommRing R] [IsKockLawvere_one R]
 
-variable {n : ℕ} (k : Fin n → ℕ) (f : (Fin n → R) → R) (d : Π i, 𝔻 R (k i)) (r : Fin n → R)
+variable {n m : ℕ} [h : Fact (m ≤ n)] (k : Fin m → ℕ)
+  (f : (Fin n → R) → R) (d : Π i, 𝔻 R (k i)) (r : Fin n → R)
+
+instance : Fact (m + 1 ≤ n + 1) := ⟨Nat.add_le_add_right h.1 1⟩
+instance : Fact (m ≤ n + 1) := ⟨le_add_right_of_le h.1⟩
+instance : Fact (0 ≤ n) := ⟨Nat.zero_le n⟩
+instance : Fact (n ≤ n) := ⟨le_rfl⟩
 
 omit [IsKockLawvere_one R] in
-instance : HAdd (Fin n → R) (Π i, 𝔻 R (k i)) (Fin n → R) where
+instance : HAdd (Fin m → R) (Π i, 𝔻 R (k i)) (Fin m → R) where
   hAdd := fun r d i ↦ r i + d i
 
 omit [IsKockLawvere_one R] in
 @[simp]
-lemma R_add_D_fun (i : Fin n) : (r + d) i = r i + d i := rfl
+lemma R_add_D_fun (r : Fin m → R) (i : Fin m) : (r + d) i = r i + d i := rfl
 
 omit [IsKockLawvere_one R] in
 @[simp]
-lemma init_R_add_D_fun (k : Fin (n + 1) → ℕ) (d : Π i, 𝔻 R (k i)) (r : Fin (n + 1) → R) :
+lemma init_R_add_D_fun (k : Fin (m + 1) → ℕ) (d : Π i, 𝔻 R (k i)) (r : Fin (m + 1) → R) :
     init (r + d) = init r + init d := rfl
 
-noncomputable abbrev Ψ (i : Fin n) := ∂_[i]^[k i]f
+noncomputable def Ψl (i : Fin m) := ∂_[castLE h.1 i]^[k i]f
 
-noncomputable def mixed_partial_deriv : (Fin n → R) → R := foldl n (Ψ k) f
+lemma Ψl_def (i : Fin m) : Ψl k f i = ∂_[castLE h.1 i]^[k i]f := rfl
+
+@[simp]
+lemma Ψl_zero (k : Fin (m + 1) → ℕ) (f : (Fin (n + 1) → R) → R) : Ψl k f 0 = ∂_[0]^[k 0]f := by
+  rfl
+
+lemma Ψl_castSucc (k : Fin (m + 1) → ℕ) (i : Fin m) (f : (Fin (n + 1) → R) → R) :
+    Ψl k f i.castSucc = Ψl (init k) f i := rfl
+
+noncomputable def mixed_partial_deriv : (Fin n → R) → R := foldl m (Ψl k) f
 
 notation3:max "∂[" k "]" f:max => mixed_partial_deriv k f
 
+lemma mixed_partial_deriv_def (f : (Fin n → R) → R) : ∂[k] f = foldl m (Ψl k) f := rfl
+
+noncomputable def Ψr (i : Fin m) (f : (Fin n → R) → R) := ∂_[castLE h.1 i]^[k i]f
+
+lemma Ψr_def (i : Fin m) : Ψr k i f = ∂_[castLE h.1 i]^[k i]f := rfl
+
+lemma Ψr_castSucc (k : Fin (m + 1) → ℕ) (i : Fin m) (f : (Fin (n + 1) → R) → R) :
+    Ψr k i.castSucc f = Ψr (init k) i f := rfl
+
+lemma mixed_partial_deriv_foldr (f : (Fin n → R) → R) : ∂[k] f = foldr m (Ψr k) f := by
+  rw [mixed_partial_deriv_def, foldl_eq_finRange_foldl, foldr_eq_finRange_foldr,
+    List.foldr_eq_foldl_reverse]
+  refine List.Perm.foldl_eq' (List.reverse_perm _).symm (fun i hi j hj f ↦ ?_) _
+  simp only [Ψl_def]
+  exact partial_deriv_iterate_comm f (castLE Fact.out j) (castLE Fact.out i) _ _
+
+theorem mixed_partial_deriv_succ_eq_deriv_init (k : Fin (m + 1) → ℕ) (f : (Fin (n + 1) → R) → R) :
+    ∂[k] f = ∂_[castLE (Nat.add_le_add_right h.1 1) (last m)]^[k (last m)]∂[init k]f := by
+  simp_rw [mixed_partial_deriv_def, foldl_succ_last, Ψl_castSucc, ← mixed_partial_deriv_def,
+    Ψl_def]
+
+theorem mixed_partial_deriv_succ_eq_init_deriv (k : Fin (m + 1) → ℕ) (f : (Fin (n + 1) → R) → R) :
+    ∂[k] f = ∂[init k]∂_[castLE (Nat.add_le_add_right h.1 1) (last m)]^[k (last m)]f := by
+  simp_rw [mixed_partial_deriv_foldr (m := m + 1), foldr_succ_last]
+  conv => enter [1, 2, x, f]; rw [Ψr_castSucc k x f]
+  rw [← mixed_partial_deriv_foldr, Ψr_def]
+
 @[simp]
-theorem mixed_partial_deriv_zero_var {k : Fin 0 → ℕ} {f : (Fin 0 → R) → R} :
-    ∂[k] f = f :=
+theorem mixed_partial_deriv_zero (k : Fin 0 → ℕ) (f : (Fin n → R) → R) : ∂[k] f = f :=
   foldl_zero _ _
 
-theorem mixed_partial_deriv_succ {n : ℕ} {k : Fin (n + 1) → ℕ} {f : (Fin (n + 1) → R) → R} :
-    ∂[k] f = foldl n (fun g i => ∂_[i.succ]^[k i.succ] g) (∂_[0]^[k 0] f) :=
-  foldl_succ _ _
-
-theorem mixed_partial_deriv_succ_last {n : ℕ} {k : Fin (n + 1) → ℕ} {f : (Fin (n + 1) → R) → R} :
-    ∂[k] f = ∂_[last n]^[k (last n)] (foldl n (fun g i ↦ ∂_[i.castSucc]^[k i.castSucc] g) f) :=
-  foldl_succ_last _ _
-
-theorem mixed_partial_deriv_one_var {k : Fin 1 → ℕ} {f : (Fin 1 → R) → R} :
-    ∂[k] f = ∂_[0]^[k 0] f := by
-  rw [mixed_partial_deriv_succ, foldl_zero]
-
 @[simp]
-theorem mixed_partial_deriv_zero {n : ℕ} {f : (Fin n → R) → R} : ∂[0] f = f := by
-  suffices h : ∀ m, foldl m (fun (a : (Fin n → R) → R) _ ↦ a) f = f by exact h n
-  intro m
-  induction m with
+theorem mixed_partial_deriv_one (k : Fin 1 → ℕ) (f : (Fin (n + 1) → R) → R) :
+    ∂[k] f = ∂_[0]^[k 0]f := by
+  simp [mixed_partial_deriv_succ_eq_deriv_init]
+
+private lemma mixed_partial_deriv_comm_partial_deriv_aux :
+    ∀ {n m : ℕ} [Fact (m ≤ n)] (k : Fin m → ℕ) (f : (Fin n → R) → R) (i : Fin n),
+    ∂[k] (∂_[i] f) = ∂_[i] (∂[k] f)
+| _, 0, _, k, f, i => by simp
+| n + 1, m + 1, hle, k, f, i => by
+  haveI hm : Fact (m ≤ n) := ⟨Nat.le_of_succ_le_succ hle.out⟩
+  simpa [mixed_partial_deriv_succ_eq_deriv_init,
+    mixed_partial_deriv_comm_partial_deriv_aux (init k) f i] using
+    partial_deriv_iterate_comm _ (castLE (Nat.add_le_add_right hm.out 1) (last m)) i (k (last m)) 1
+
+lemma mixed_partial_deriv_comm_partial_deriv_iterate (i : Fin n) (j : ℕ) :
+    ∂[k] (∂_[i]^[j]f) = ∂_[i]^[j](∂[k] f) := by
+  induction j with
   | zero => simp
-  | succ m ih => simp [foldl_succ, ih]
+  | succ j ih =>
+    rw [Function.iterate_succ', Function.comp_def, mixed_partial_deriv_comm_partial_deriv_aux, ih]
+
+lemma mixed_partial_deriv_comm_partial_deriv (i : Fin n) :
+    ∂[k] (∂_[i] f) = ∂_[i] (∂[k] f) := by
+  simpa [Function.iterate_one] using mixed_partial_deriv_comm_partial_deriv_iterate k f i 1
+
+lemma partial_deriv_iterate_castSucc_snoc (f : (Fin (n + 1) → R) → R) (i : Fin n) (s : Fin n → R)
+    (c : R) (j : ℕ) : ∂_[castSucc i]^[j] f (snoc s c) = ∂_[i]^[j] (fun t ↦ f (snoc t c)) s := by
+  induction j generalizing f s with
+  | zero => simp
+  | succ j ih =>
+    simp only [Function.iterate_succ', Function.comp_def, ← partial_deriv_snoc_castSucc]
+    congr; ext; exact ih f _
+
+lemma mixed_partial_deriv_snoc : ∀ {n m : ℕ} [Fact (m ≤ n)] [Fact (m ≤ n + 1)] (k : Fin m → ℕ)
+    (f : (Fin (n + 1) → R) → R) (s : Fin n → R) (c : R),
+    ∂[k] f (snoc s c) = ∂[k] (fun t ↦ f (snoc t c)) s
+| _, 0, _, _, _, _, _, _ => by simp
+| n + 1, m + 1, hle, hle', k, f, s, c => by
+  have hm : Fact (m ≤ n) := ⟨Nat.le_of_succ_le_succ hle.1⟩
+  have hm' : Fact (m ≤ n + 1) := ⟨le_add_right_of_le hm.1⟩
+  have key : (fun t ↦ ∂[init k] f (snoc t c)) = ∂[init k] (fun t ↦ f (snoc t c)) := by
+    ext; exact mixed_partial_deriv_snoc ..
+  have : castLE (Nat.add_le_add_right hm'.1 1) _ =
+    castSucc (castLE (Nat.add_le_add_right hm.1 1) (last m)) := rfl
+  rw [mixed_partial_deriv_succ_eq_deriv_init, this, partial_deriv_iterate_castSucc_snoc, key,
+    ← mixed_partial_deriv_succ_eq_deriv_init]
+
+theorem mixed_partial_deriv_initial (k : Fin (n + 1) → ℕ) (f : (Fin (n + 1) → R) → R)
+  (r : Fin (n + 1) → R) : ∂[k] f r =
+    letI g : (Fin n → R) → R := fun s ↦ ∂^[k (last n)] (fun y ↦ f (snoc s y)) (r (last n))
+    ∂[init k]g (init r) := by
+  set g : (Fin n → R) → R := fun s ↦ ∂^[k (last n)] (fun y ↦ f (snoc s y)) (r (last n)) with hg
+  simp_rw [← partial_deriv_iterate_snoc_last] at hg
+  rw [hg]
+  conv_lhs =>
+    rw [mixed_partial_deriv_succ_eq_init_deriv, ← snoc_init_self r, mixed_partial_deriv_snoc]
+  rfl
 
 variable [Divisible R] [IsKockLawvere R]
 
@@ -103,14 +183,10 @@ lemma helper [AddCommGroup T] (k : Fin (n + 1) → ℕ)
 
 end helper
 
-example (a₁ a₂ b c d : R) : a₁ * a₂ * b * c * d = (a₁ * a₂) * (b * d * c) := by
-  rw [mul_assoc (a₁ * a₂), mul_assoc (a₁ * a₂)]
-  sorry
-
-theorem taylor_multi : ∀ {n} (k : Fin n → ℕ) (f : (Fin n → R) → R) (d : Π i, 𝔻 R (k i))
-    (r : Fin n → R), f (r + d) = ∑ (α : Iic k), ∂[α]f r * ∏ i, (d i) ^ (α.1 i) * ⅟((α.1 i)! : R)
+theorem taylor_multi_aux : ∀ {n} (k : Fin n → ℕ) (f : (Fin n → R) → R) (d : Π i, 𝔻 R (k i))
+    (r : Fin n → R), f (r + d) = ∑ (α : Iic k), ∂[α.1]f r * ∏ i, (d i) ^ (α.1 i) * ⅟((α.1 i)! : R)
 | 0 => fun k f d r ↦ by
-  simp only [univ_eq_attach, mixed_partial_deriv_zero_var, univ_eq_empty, prod_empty, mul_one,
+  simp only [univ_eq_attach, mixed_partial_deriv_zero, univ_eq_empty, prod_empty, mul_one,
     Finset.sum_const, card_attach, nsmul_eq_mul]
   convert (one_mul _).symm
   exact cast_one
@@ -126,11 +202,11 @@ theorem taylor_multi : ∀ {n} (k : Fin n → ℕ) (f : (Fin n → R) → R) (d 
   have hh : ∀ i, ∂_[last n]^[i] f (snoc (init (r + d)) (r (last n))) = h i (init (r + d)) :=
     fun _ ↦ rfl
   simp_rw [hfg, taylor_k g _ (k (last n)), ← hg, hh, init_R_add_D_fun,]
-  conv => enter [1, 2, x, 1, 1]; exact taylor_multi (init k) ..
+  conv => enter [1, 2, x, 1, 1]; exact taylor_multi_aux (init k) ..
   simp_rw [Finset.sum_mul, helper, foo]
   congr
   ext α
-  rw [mul_rotate _ (∏ _, _), mul_comm (∂[α]f r), mul_right_comm, mul_right_comm (∏ _, _), mul_comm
+  rw [mul_rotate _ (∏ _, _), mul_comm (∂[α.1]f r), mul_right_comm, mul_right_comm (∏ _, _), mul_comm
     (∏ _, _), prod_mul_distrib, mul_comm (∏ _, _), ← mul_assoc, mul_assoc (_ * (∏ _, _)),
     mul_assoc (_ * (∏ _, _)), prod_mul_distrib, mul_comm (∏ _, _) (∏ _, _), mul_assoc (∏ _, _)]
   congr 1
@@ -138,6 +214,15 @@ theorem taylor_multi : ∀ {n} (k : Fin n → ℕ) (f : (Fin n → R) → R) (d 
     rfl
   rw [prod_univ_castSucc, mul_assoc]
   congr 2
-  sorry
+  simp [h, partial_deriv_iterate_snoc_last, mixed_partial_deriv_initial]
+
+theorem taylor_multi (k : Fin n → ℕ) (f : (Fin n → R) → R) (d : Π i, 𝔻 R (k i))
+    (r : Fin n → R) : f (r + d) =
+    ∑ α ∈ Iic k, ∂[α]f r * ∏ i, (d i) ^ (α i) * ⅟((α i)! : R) := by
+  convert taylor_multi_aux k f d r
+  rw [Finset.sum_subtype]
+  intro α
+  rfl
+
 
 end SDG
