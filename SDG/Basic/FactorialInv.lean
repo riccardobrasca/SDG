e0580b7c5ff2fa0126579b438fa91c15f11b96a1
Riccardo Brasca <riccardo.brasca@gmail.com>
2026-03-13 11:59:06 +0100

    better

--- SDG/Basic/FactorialInv.lean
+++ SDG/Basic/FactorialInv.lean
@@ -10,25 +10,27 @@ open Nat
 
 variable (R : Type*) [CommRing R]
 
-/-- A typeclass asserting that all nonzero natural numbers are invertible in `R`. -/
+lemma succ_mul_inv_factorial_succ (k : ℕ) [Invertible (((k + 1)! : R))]
+    [Invertible (((k ! : ℕ) : R))] : ⅟↑(k + 1)! * (↑(k + 1)) = ⅟(k ! : R) := by
+  simp [invOf_mul_eq_iff_eq_mul_left, factorial_succ]
+
+/-- A typeclass asserting that the factorial of all natural numbers is invertible in `R`. -/
 class Divisible where
-  /-- Every nonzero natural number is invertible in `R`. -/
-  divisible : ∀ {n : ℕ}, n ≠ 0 → Invertible (n : R)
+  /-- `n !` invertible in `R`. -/
+  divisible : ∀ {n : ℕ}, Invertible ((n !) : R)
 
 variable [Divisible R]
 
 instance (n : ℕ) : Invertible (n ! : R) :=
-  Divisible.divisible (factorial_ne_zero n)
+  Divisible.divisible
+
+instance : Invertible (1 : R) := invertibleOne
 
 lemma inv_factorial_zero : ⅟(0! : R) = 1 := by
-  have : Invertible (1 : R) := invertibleOne
   simp
 
 lemma inv_factorial_one : ⅟(1! : R) = 1 := by
   have : Invertible (1 : R) := invertibleOne
   simp
-
-lemma succ_mul_inv_factorial_succ (k : ℕ) : ⅟↑(k + 1)! * (↑(k + 1)) = ⅟(k ! : R) := by
-  simp [invOf_mul_eq_iff_eq_mul_left, factorial_succ]
 
 end SDG
