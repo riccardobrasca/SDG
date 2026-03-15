9ebb68dfff04020bd40f7f92d5f71f89532bdae6
Riccardo Brasca <riccardo.brasca@gmail.com>
2026-03-15 12:36:14 +0100

    useless

--- SDG.lean
+++ SDG.lean
@@ -6,7 +6,6 @@ import SDG.Basic.Defs
 import SDG.ForMathlib.Fin
 import SDG.IsKockLawvere.Taylor
 import SDG.IsKockLawvere.TaylorMulti
-import SDG.IsKockLawvere_one.Alpha
 import SDG.IsKockLawvere_one.Basic
 import SDG.IsKockLawvere_one.Deriv
 import SDG.IsKockLawvere_one.EM
