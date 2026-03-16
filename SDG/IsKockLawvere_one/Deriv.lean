58d7b4560db5c4d69879093870ec454a42ffc4f8
Riccardo Brasca <riccardo.brasca@gmail.com>
2026-03-16 12:10:58 +0100

    move stuff

--- SDG/IsKockLawvere_one/Deriv.lean
+++ SDG/IsKockLawvere_one/Deriv.lean
@@ -110,6 +110,21 @@ theorem deriv_equiv (f : R ≃ R) : ∂↑f⁻¹ * ∂f ∘ ↑f⁻¹ = 1 := by
   rw [chain_rule_fun, funext_iff] at this
   simpa using this (f.invFun x)
 
+section taylor
+
+theorem taylor_two_aux [Invertible (2 : R)] (f : R → R) (x : R) (d₁ d₂ : D R) :
+    letI δ : R := d₁ + d₂
+    f (x + δ) = f x + ∂f x * δ + ∂∂f x * δ ^ 2 * ⅟2 :=
+  calc f (x + (d₁ + d₂)) = f (x + d₁ + d₂) := by rw [add_assoc]
+       _ = f (x + d₁) + ∂f (x + d₁) * d₂ := by rw [taylor_one f]
+       _ = f x + ∂f x * d₁ + ∂f (x + d₁) * d₂ := by rw [taylor_one f]
+       _ = f x + ∂f x * d₁ + (∂f x + ∂∂f x * d₁) * d₂ := by rw [taylor_one ∂f]
+       _ = f x + ∂f x * (d₁ + d₂) + ∂∂f x * (d₁ * d₂) := by ring
+       _ = f x + ∂f x * (d₁ + d₂) + ∂∂f x * ((d₁ + d₂) ^ 2 * ⅟2) := by rw [D_add_sq_dvd_two]
+       _ = _ := by ring
+
+end taylor
+
 end IsKockLawvere_one
 
 end SDG
